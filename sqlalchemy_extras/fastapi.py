4aa31a677f61e2962e7d2f9efdf6ac69c8de60bd
Francisco Del Roio <francipvb@hotmail.com>
2022-07-25 14:42:40 -0300

    Fixed a problem in the sqlalchemy session.

--- sqlalchemy_extras/fastapi.py
+++ sqlalchemy_extras/fastapi.py
@@ -14,7 +14,7 @@ from typing import Any, AsyncGenerator, Optional, Union
 from fastapi import Depends, FastAPI, Request
 from sqlalchemy import func
 from sqlalchemy.engine.url import URL
-from sqlalchemy.exc import ProgrammingError
+from sqlalchemy.exc import DatabaseError, ProgrammingError
 from sqlalchemy.ext.asyncio import (
     AsyncConnection,
     AsyncEngine,
@@ -158,7 +158,12 @@ async def sqlalchemy_session(
     session = factory()
 
     async with session:
-        yield session
+        try:
+            yield session
+        except DatabaseError:
+            raise
+        else:
+            await session.commit()
 
 
 async def sqlalchemy_connection(
