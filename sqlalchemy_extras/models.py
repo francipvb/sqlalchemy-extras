ca977b7a14a085e54a25e6e8746f35362f3a7c40
Francisco Del Roio <francipvb@hotmail.com>
2023-04-11 15:36:52 -0300

    Fixed `WithPK` and `Timestamps` mixins.

--- sqlalchemy_extras/models.py
+++ sqlalchemy_extras/models.py
@@ -1,7 +1,8 @@
 # pylint: disable=not-callable
+from datetime import datetime
 import typing
 
-from sqlalchemy import TIMESTAMP, Boolean, false, func, Column, orm
+from sqlalchemy import TIMESTAMP, false, func, orm
 from sqlalchemy.types import TypeEngine
 
 from .utils import pluralize
@@ -80,17 +81,15 @@ class Timestamps:
 
     __abstract__ = True
 
-    created = Column(
-        TIMESTAMP(),
+    created: orm.Mapped[datetime] = orm.mapped_column(
+        type_=TIMESTAMP(),
         server_default=func.current_timestamp(),
-        nullable=False,
         comment="When a row were added.",
     )
-    updated = Column(
-        TIMESTAMP(),
+    updated: orm.Mapped[datetime | None] = orm.mapped_column(
+        type_=TIMESTAMP(),
         onupdate=func.current_timestamp(),
         comment="Last row update date and time.",
-        nullable=True,
         default=None,
     )
 
@@ -112,7 +111,7 @@ class Timestamps:
         <sqlalchemy.sql.functions.current_timestamp at ...>
         >>>
         """
-        self.updated = func.current_timestamp()
+        self.updated = func.current_timestamp()  # type: ignore
 
 
 class Autonamed:
@@ -161,7 +160,7 @@ class WithPK(typing.Generic[_TId]):
         typing.Optional[typing.Mapping[str, typing.Any]]
     ] = None
 
-    # id: orm.Mapped[_TId]
+    id: orm.Mapped[_TId]
 
     def __init_subclass__(cls, *args: typing.Any, **kwargs: typing.Any) -> None:
         orig_bases: typing.Tuple[typing.Any, ...] = cls.__orig_bases__  # type: ignore
